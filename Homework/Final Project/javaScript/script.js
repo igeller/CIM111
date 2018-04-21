d6fa6ac5735feed3b582a4ef9a2fd2dc894409b9
IG <ieg23@miami.edu>
2018-04-21 16:41:08 -0400

    menu toggle and responsive working

---
+++ Homework/Final Project/javaScript/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $(window).ready(menu);
+
+    var browserHeight = $(window).height();
+    var browserWidth = $(window).width();
+
+
+    console.log($(window).resize());
+
+    $(".fullPage").height(browserHeight);
+
+
+    $(window).resize(menu);
+
+    function menu() {
+        $(".mobileNav").hide();
+        if($(window).width() <= 860 && $(".mobileNav").css("display", "hide")){
+            $(".desktopNav").hide();
+            $("#navButton").click(function () {
+                $(this).css("background-color", "grey");
+                $(".mobileNav").toggle();
+            });
+        }else {
+            $(".desktopNav").show();
+        }
+    }//end responsiveMenu
+
+
+
+
+
+
+
+
+
+
+});//end of code
