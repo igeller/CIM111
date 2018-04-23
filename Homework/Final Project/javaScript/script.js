cfe105a940e5304f7899ab9658d5e2fe11837d3a
IG <ieg23@miami.edu>
2018-04-22 21:24:10 -0400

    got rid of unecessary folder

--- Homework/Final Project/javaScript/script.js
+++ Homework/Final Project/javaScript/script.js
@@ -25,6 +25,38 @@ $(document).ready(function(){
     }//end responsiveMenu
 
 
+    //swipe panels for the first page
+    var controller = new ScrollMagic.Controller();
+    var swipe = new TimelineMax()
+        .fromTo("section.panel#p2", 1, {x:"100%"}, {x:"-25%", ease: Linear.easeNone});
+
+    var navHeight = $("#navButton").height();
+    new ScrollMagic.Scene({
+        triggerElement: "#home",
+        triggerHook:"onLeave",
+        duration: "1000%",
+        offset: -navHeight
+    }).setPin("#home")
+      .setTween(swipe)
+      .addIndicators({name: "panel slides"})
+      .addTo(controller);
+
+
+
+     var scene2 =  new ScrollMagic.Scene({
+          triggerElement: "#p2",
+          duration:"100%"
+      }).setPin("#aboutH1")
+        .addIndicators({name: "h1 tag"})
+        .addTo(controller);
+
+      //scroll between first and second PAGE
+
+
+
+
+
+
 
 
 
